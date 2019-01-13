f86106e73e4c26ea58d4e5cf65c7631a1bc0d27b
EberthFelipe <eberthfelipe@gmail.com>
2019-01-13 17:07:52 -0400

    Click effect for clickable views

--- app/src/main/java/udacity/android/com/bakingapp/view/RecipeDetailStepFragment.java
+++ app/src/main/java/udacity/android/com/bakingapp/view/RecipeDetailStepFragment.java
@@ -1,13 +1,11 @@
 package udacity.android.com.bakingapp.view;
 
-import android.app.Activity;
 import android.content.Context;
 import android.databinding.DataBindingUtil;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
